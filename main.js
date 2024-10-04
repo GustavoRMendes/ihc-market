99065db78b301e875d73e833d249be789e49d966
Gustavo-Mendess <gustavormendess@gmail.com>
2024-10-03 22:33:58 -0400

    Adicionar console log nos seletores

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 const botoes = document.querySelectorAll(".botoes");
 const listas = document.querySelectorAll(".listas");
 const seletor = document.querySelector("select");
+const options = document.querySelectorAll("option");
+console.log(seletor);
+console.log(options);
+console.log(listas);
+options.forEach((item) => console.log(item.textContent));
